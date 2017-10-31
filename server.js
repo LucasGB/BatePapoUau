575cf29d7a8709817b521a1eff22cea0e7deac33
eliveltonFernando2016 <eliveltonfoliveira@gmail.com>
2017-10-31 10:39:14 -0200

    Tela home/login feita!

--- server.js
+++ server.js
@@ -7,16 +7,15 @@ server.listen(3000);
 console.log("Server started, listening on port 3000...");
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/home.html');
 });
 
 io.on('connection', function (socket) {
-	console.log("New connection");
+    console.log("New connection");
 
+    socket.emit('Welcome', {data: "Connected."});
 
-  socket.emit('Welcome', {data: "Connected."});
-
-  socket.on('join', function(data) {
+    socket.on('join', function (data) {
         console.log(data);
-  });
+    });
 });
