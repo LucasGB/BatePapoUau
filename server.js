037172d8ba341b4d5b2218f4b34e99e89f283c65
lucasgb <lucasguedesbarboza@gmail.com>
2017-11-09 13:01:37 -0200

    Added static file serving and dynamic namespacing

--- server.js
+++ server.js
@@ -3,19 +3,29 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+app.use(express.static(__dirname + '/public'));
+
 server.listen(3000);
 console.log("Server started, listening on port 3000...");
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/home.html');
+    res.sendFile(__dirname + '/public/home.html');
+});
+
+app.get('/:id', function (req, res) {
+	res.sendFile(__dirname + '/public/room.html');
 });
 
 io.on('connection', function (socket) {
     console.log("New connection");
 
-    socket.emit('Welcome', {data: "Connected."});
-
-    socket.on('join', function (data) {
-        console.log(data);
+    socket.on('join_room', function (room) {
+  		socket.join(room);
+  		console.log('User joined room #'+room);
     });
+
+    socket.on('disconnect', function (data) { 
+    	console.log('User disconnected.');
+    });
+
 });
